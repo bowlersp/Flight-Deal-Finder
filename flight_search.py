d6a04ee3a76ef3f152ff8503f4a07bf44de957aa
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-05-03 23:05:56 -0500

    Create flight_search.py

---
+++ flight_search.py
@@ -0,0 +1,11 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com/"
+TEQUILA_API_KEY = "2WlxUYZeY0I8afdSzyqHaaAIYoO0NYPg"
+
+class FlightSearch:
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
