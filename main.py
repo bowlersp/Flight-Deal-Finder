ca729547c87945b7be165a63d76822f5c8a42b4d
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-05-06 22:45:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -39,4 +39,8 @@ for destination in sheet_data:
         # notification_manager.send_sms(
         #     message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
         # )
-        notification_manager.send_email()
+        # message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        # print(message)
+        notification_manager.send_email(
+            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
